759f0e33e64ea230c84925f26670ce0871708d12
Oni Noor <muhammadsyeikhooninoor2000@mail.ugm.ac.id>
2026-01-22 09:13:19 +0700

    feat: Configure Tailwind CSS and add TypeScript path aliases.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
-import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), partytown()],
   adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()],
   },
